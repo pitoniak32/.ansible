19595b4335f87014acbcb425cce78e0e884cb2ae
David Pitoniak <davidpitoniak32@gmail.com>
2023-10-19 16:53:41 -0400

    feat: initial implementation with ansible-vault

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@
 
 # pip3 install ansible
 # ansible-galaxy install -r requirements.yml
-ansible-playbook playbook.yml -Kvv --tags=vault --ask-vault-pass
+ansible-playbook playbook.yml -Kvv --tags=vault --skip-tags=work --vault-id personal@prompt
