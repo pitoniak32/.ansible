8ae1103c6e2fd915263589979076b26d808207f2
David Pitoniak <davidpitoniak32@gmail.com>
2023-10-20 15:54:27 -0400

    feat: small updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@
 
 # pip3 install ansible
 # ansible-galaxy install -r requirements.yml
-ansible-playbook playbook.yml -Kvv --tags=vault --skip-tags=work --vault-id personal@prompt
+ansible-playbook playbook.yml -Kvv --skip-tags=work --vault-id personal@prompt
