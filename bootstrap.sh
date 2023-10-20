d93d321df720197a0c4198bba17c1e154129efdf
David Pitoniak <davidpitoniak32@gmail.com>
2023-10-20 16:24:57 -0400

    fix: ssh config

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,8 @@
 
 # pip3 install ansible
 # ansible-galaxy install -r requirements.yml
-ansible-playbook playbook.yml -Kvv --skip-tags=work --vault-id personal@prompt
+ansible-playbook playbook.yml -Kvv \
+  --skip-tags=work \
+  --vault-id personal@prompt \
+  # --vault-id work@prompt
+  # --tags=
